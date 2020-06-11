7f65417f85335428c30784b9d4d7a08eb1e40208
Mitchell Kotler <mitch@mitchellkotler.net>
2020-06-11 12:02:56 -0400

    clean up

--- setup.py
+++ setup.py
@@ -1,32 +1,46 @@
-# Third Party
 from setuptools import setup
 
 setup(
-    name='python-documentcloud',
-    version='1.1.1',
+    name='python-documentcloud2',
+    version='2.0.0',
     description='A simple Python wrapper for the DocumentCloud API',
-    author='Ben Welsh',
-    author_email='ben.welsh@gmail.com',
-    url='http://datadesk.github.com/python-documentcloud/',
+    author='Mitchell Kotler',
+    author_email='mitch@muckrock.com',
+    url='https://github.com/muckrock/python-documentcloud',
     license="MIT",
     packages=("documentcloud",),
-    test_suite="tests.test_all",
     include_package_data=True,
     install_requires=(
-        'python-dateutil>=2.1',
-        'six>=1.4.1',
-        'rfc3987',
+        'listcrunch',
+        'python-dateutil',
+        'ratelimit',
+        'requests',
+        'urllib3',
     ),
+    extras_require={
+        'dev': [
+            'black',
+            'coverage',
+            'isort',
+            'pylint',
+            'pytest',
+            'pytest-mock',
+            'pytest-recording',
+            'sphinx',
+            'twine',
+            'vcrpy',
+        ]
+    },
     classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
     )
 )
